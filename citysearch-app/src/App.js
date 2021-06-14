889d4bfe623e01fb135380239bd3c401d3563ed7
Bryant Saavedra <79132595+BryantS42@users.noreply.github.com>
2021-06-13 23:06:39 -0400

    Fixed previous issues.

--- citysearch-app/src/App.js
+++ citysearch-app/src/App.js
@@ -4,80 +4,72 @@ import './App.css';
 class App extends Component{
   constructor(props){
     super(props);
-    this.citySearch = this.citySearch.bind(this);
     this.state = {
       items: [],
       isLoaded: false,
-      zipitems: [],
     }
   }
 
-  citySearch(e){
-    let city = e.target.value.toLocaleUpperCase();    //on screen it looks lowercase but it retrieves results
-    try{
-      console.log("fetching");
+
+  promisePractice = (e) => {
+    
+    let city = e.target.value.toLocaleUpperCase(); 
     fetch('https://ctp-zip-api.herokuapp.com/city/'+ city)
-        .then(res => res.json())
-        .then(json => {
-          this.setState({
-            items: json,
-          });
-        });
-    if(this.state.items != null){
-      this.setState({isLoaded:true})
-      
-    }
-    else{this.setState({isLoaded:false})}
-  }catch(e){console.error(e);}
-        
-  }
-
-  zipSearch(zipcode){
-    console.log(zipcode)
-    fetch('http://ctp-zip-api.herokuapp.com/zip/'+ zipcode)
     .then(res => res.json())
-        .then(json => {
-          this.setState({
-            zipitems: json,
-          });
-        });
-  }
+    .then(zipCodes => {
+      let arrayC = [];
+      const req = zipCodes.map((zip) =>{
+        return fetch('https://ctp-zip-api.herokuapp.com/zip/'+ zip)
+          .then(response => response.json())
+          .then(zipdata => {
+            arrayC = arrayC.concat(zipdata)        
+            console.log(arrayC)
+          }).catch(error => console.log(error))
+      })
+      return Promise.all(req).then(() => {
+        arrayC.sort((a, b) => (a.Zipcode > b.Zipcode) ? 1 : -1)
+        console.log(arrayC)
+        this.setState({
+          items: arrayC,
+          isLoaded: true,
+        } )
+      })
+    })
+    }
 
   render(){
-    const search = <input id="citySearch" type="text" placeholder="Search cities" onChange={this.citySearch}/>
-    var{ isLoaded, items, zipitems} = this.state;
+      const search = <input id="citySearch" type="text" placeholder="Search cities" onChange={this.promisePractice}/>
+      var{ isLoaded, items} = this.state;
 
-    if(!isLoaded){
-      return (
-        <div className="App">
-        <h1>City Search</h1>
-        <div id="searchBox">~{search}~</div>
-        <p>No results</p>
-        </div>)
-    }
-    else{
-      return(
+      if(!isLoaded){
+        return (
           <div className="App">
-            <h1>City Search</h1>
-              <div id="searchBox">~{search}~</div>
-              <ul>
-                {items.map(item => (
-                  <li id="list" key={item.id}>
-                    {this.zipSearch(item)}
-                  {/*  {zipitems.map(newItems => (
-                      <li>
-                      <li id="item" key={newItems.id}>State: {newItems.State}</li>
-                      <li id="item" key={newItems.id}>Total Wages: {newItems.TotalWages}</li>
-                      </li>
-                  ))} */}
-                    {item}
-                </li>
-                ))}
-              </ul>
-          </div>
-      )
-    }
-}
+          <h1>City Search</h1>
+          <div id="searchBox">~{search}~</div>
+          <p>No results</p>
+          </div>)
+      }
+      else{
+        
+        return(
+            <div className="App">
+              <h1>City Search</h1>
+                <div id="searchBox">~{search}~</div>
+                <ul>
+                  {items.map(item => (
+                    <li id="list" key={item.id}>
+                      <div>State: {item.State}</div>
+                      <div>Area: {item.Zipcode}</div>
+                      <div>Total wages: {item.TotalWages}</div>
+                      <div>Population (estimated): {item.EstimatedPopulation} </div>
+                      <div>Location: ({item.Lat},{item.Long})</div>
+                  </li>
+                  ))} 
+                </ul>
+            </div>
+        )
+      }
+  }
 }
 
 export default App;
